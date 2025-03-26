c35de699420512a934226753c8766f8ebd199327
Ahoorast <AhooraBozorgManesh@gmail.com>
2025-03-27 00:06:07 +0330

    add embedding column

--- Artemis/src/lib/server/db/schema.ts
+++ Artemis/src/lib/server/db/schema.ts
@@ -1,6 +1,7 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, vector } from 'drizzle-orm/pg-core';
 
 export const genres = pgTable("genre", {
 	id: serial().primaryKey(),
 	name: text().unique().notNull(),
+	embedding: vector('embedding', { dimensions: 512 })
 });
